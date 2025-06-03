a1649874acebeacda9c3eef10ddfe1aabf12d1cf
CTLR-X <Bohdan.Kuspys@student.kdg.be>
2025-06-03 21:17:57 +0200

    Working Lovyan

--- src/main.cpp
+++ src/main.cpp
@@ -1,179 +1,42 @@
 #include <Arduino.h>
 #include <LovyanGFX.h>
-#include <lvgl.h>
 #include "LGFX_ESP32S3_RGB_TFT_SPI_ST7701_GT911.h"
 
-/*Change to your screen resolution*/
-static const uint16_t screenWidth  = 480;
-static const uint16_t screenHeight = 480;
-
-static lv_draw_buf_t draw_buf;
-static lv_color_t buf[2][ screenWidth * 10 ];
-
 LGFX gfx;
-lv_obj_t *led;
-boolean led_status = true;
-
-/* Display flushing */
-void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t *color_p )
-{
-    if (gfx.getStartCount() == 0)
-    {   // Processing if not yet started
-        gfx.startWrite();
-    }
-    gfx.pushImage( area->x1
-                 , area->y1
-                 , area->x2 - area->x1 + 1
-                 , area->y2 - area->y1 + 1
-                 , ( uint16_t* )color_p);
-    lv_display_flush_ready( disp );
-}
-
-/*Read the touchpad*/
-void my_touchpad_read( lv_indev_t * indev_driver, lv_indev_data_t * data )
-{
-    uint16_t touchX, touchY;
-
-    data->state = LV_INDEV_STATE_REL;
-
-    if( gfx.getTouch( &touchX, &touchY ) )
-    {
-        data->state = LV_INDEV_STATE_PR;
-
-        /*Set the coordinates*/
-        data->point.x = touchX;
-        data->point.y = touchY;
-    }
-}
-
-static void btn_event_cb(lv_event_t *e)
-{
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t *btn = (lv_obj_t*)lv_event_get_target(e);
-    if (code == LV_EVENT_CLICKED)
-    {
-        static uint8_t cnt = 0;
-        cnt++;
-
-        lv_obj_t *label = lv_obj_get_child(btn, 0);
-        lv_label_set_text_fmt(label, "COUNTER: %d", cnt);
-        if (led_status)
-        {
-            lv_led_off(led);
-            led_status = false;
-        } else {
-            lv_led_on(led);
-            led_status = true;
-        }
-    }
-}
-
-void lv_example_btn(void)
-{
-    /*Init the style for the default state*/
-    static lv_style_t style;
-    lv_style_init(&style);
-
-    lv_style_set_radius(&style, 3);
-
-    lv_style_set_bg_opa(&style, LV_OPA_100);
-    lv_style_set_bg_color(&style, lv_palette_main(LV_PALETTE_BLUE));
-    lv_style_set_bg_grad_color(&style, lv_palette_darken(LV_PALETTE_BLUE, 2));
-    lv_style_set_bg_grad_dir(&style, LV_GRAD_DIR_VER);
-
-    lv_style_set_border_opa(&style, LV_OPA_40);
-    lv_style_set_border_width(&style, 2);
-    lv_style_set_border_color(&style, lv_palette_main(LV_PALETTE_GREY));
-
-    lv_style_set_shadow_width(&style, 8);
-    lv_style_set_shadow_color(&style, lv_palette_main(LV_PALETTE_GREY));
-    lv_style_set_shadow_ofs_y(&style, 8);
-
-    lv_style_set_outline_opa(&style, LV_OPA_COVER);
-    lv_style_set_outline_color(&style, lv_palette_main(LV_PALETTE_BLUE));
-
-    lv_style_set_text_color(&style, lv_color_white());
-    lv_style_set_pad_all(&style, 10);
-
-    /*Init the pressed style*/
-    static lv_style_t style_pr;
-    lv_style_init(&style_pr);
-
-    /*Add a large outline when pressed*/
-    lv_style_set_outline_width(&style_pr, 30);
-    lv_style_set_outline_opa(&style_pr, LV_OPA_TRANSP);
-
-    lv_style_set_translate_y(&style_pr, 5);
-    lv_style_set_shadow_ofs_y(&style_pr, 3);
-    lv_style_set_bg_color(&style_pr, lv_palette_darken(LV_PALETTE_BLUE, 2));
-    lv_style_set_bg_grad_color(&style_pr, lv_palette_darken(LV_PALETTE_BLUE, 4));
-
-    /*Add a transition to the outline*/
-    static lv_style_transition_dsc_t trans;
-    static lv_style_prop_t props[] = {LV_STYLE_OUTLINE_WIDTH, LV_STYLE_OUTLINE_OPA};
-    lv_style_transition_dsc_init(&trans, props, lv_anim_path_linear, 300, 0, NULL);
-
-    lv_style_set_transition(&style_pr, &trans);
-
-    lv_obj_t *btn1 = lv_btn_create(lv_scr_act());
-    lv_obj_remove_style_all(btn1); /*Remove the style coming from the theme*/
-    lv_obj_add_style(btn1, &style, 0);
-    lv_obj_add_style(btn1, &style_pr, LV_STATE_PRESSED);
-    lv_obj_set_size(btn1, 380, 120);
-    lv_obj_set_pos(btn1, 50, 120);
-
-    lv_obj_add_event_cb(btn1, btn_event_cb, LV_EVENT_ALL, NULL);
-
-    lv_obj_t *label = lv_label_create(btn1);
-    lv_label_set_text(label, "COUNTER");
-    lv_obj_set_style_text_font(label, &lv_font_montserrat_14, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_center(label);
-}
 
 void setup()
 {
     gfx.begin();
+    gfx.setRotation(0);  // Set rotation if needed
+    gfx.setBrightness(128);  // Set brightness (0-255)
 
-    lv_init();
+    // Fill screen with black
+    gfx.fillScreen(TFT_BLACK);
     
-    // Initialize draw buffer
-    lv_draw_buf_init(&draw_buf, 
-                     screenWidth, 
-                     screenHeight, 
-                     LV_COLOR_FORMAT_RGB565, 
-                     screenWidth * 10,
-                     buf[0],
-                     sizeof(buf[0]));
-
-    /*Initialize the display*/
-    static lv_display_t *disp = lv_display_create(screenWidth, screenHeight);
-    lv_display_set_flush_cb(disp, my_disp_flush);
-    lv_display_set_draw_buffers(disp, &draw_buf, NULL);
-
-    /*Initialize the input device driver*/
-    static lv_indev_t *indev = lv_indev_create();
-    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
-    lv_indev_set_read_cb(indev, my_touchpad_read);
-
-    lv_obj_t *label2 = lv_label_create(lv_scr_act());
-    lv_label_set_long_mode(label2, LV_LABEL_LONG_SCROLL_CIRCULAR); /*Circular scroll*/
-    lv_obj_set_width(label2, 350);
-    lv_label_set_text(label2, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu molestie nibh. Nunc dignissim sem elementum imperdiet scelerisque. Fusce molestie eros sed orci maximus, vel iaculis quam facilisis. Donec dapibus enim elit, in luctus mauris cursus venenatis. Curabitur viverra ante id lectus suscipit, vitae molestie ");
-    lv_obj_set_style_text_color(label2, lv_color_hex(0xF00022), LV_PART_MAIN | LV_STATE_DEFAULT );
-    lv_obj_align(label2, LV_ALIGN_CENTER, 0, 40);
-
-    led = lv_led_create(lv_scr_act());
-    lv_obj_set_width(led, 50);
-    lv_obj_set_height(led, 50);
-    lv_obj_align(led, LV_ALIGN_CENTER, 0, 100);
-    lv_led_set_color(led, lv_color_hex(0x00FF00));
-    lv_led_on(led);
-
-    lv_example_btn();
+    // Draw some shapes
+    gfx.fillRect(10, 10, 100, 100, TFT_RED);  // Red square
+    gfx.fillCircle(200, 200, 50, TFT_GREEN);  // Green circle
+    gfx.fillTriangle(300, 100, 350, 200, 400, 100, TFT_BLUE);  // Blue triangle
+    
+    // Draw some text
+    gfx.setTextSize(2);
+    gfx.setTextColor(TFT_WHITE);
+    gfx.setCursor(50, 300);
+    gfx.println("Hello, LovyanGFX!");
+    
+    // Draw a gradient
+    for(int i = 0; i < gfx.width(); i++) {
+        gfx.drawFastVLine(i, 350, 50, gfx.color565(i * 255 / gfx.width(), 0, 255 - (i * 255 / gfx.width())));
+    }
 }
 
 void loop()
 {
-  lv_timer_handler(); /* let the GUI do its work */
-  delay(5);
+    // Read touch input
+    uint16_t x, y;
+    if (gfx.getTouch(&x, &y)) {
+        // Draw a small circle where touched
+        gfx.fillCircle(x, y, 5, TFT_YELLOW);
+        delay(50);  // Small delay to prevent too many touch points
+    }
 }
