7033456a6698359c368f76c3cf031cb7867f0f3f
CTLR-X <Bohdan.Kuspys@student.kdg.be>
2025-06-03 23:43:17 +0200

    Working LVGL

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -1,124 +1,297 @@
 #ifndef LV_CONF_H
 #define LV_CONF_H
 
+/* Use the correct include path for LVGL */
+#define LV_CONF_INCLUDE_SIMPLE 1
+#define LV_CONF_PATH "lv_conf.h"
+
 #include <stdint.h>
+
+/*====================
+   COLOR SETTINGS
+ *====================*/
+
+/* Color depth: 1 (I1), 8 (L8), 16 (RGB565), 24 (RGB888), 32 (XRGB8888) */
+#define LV_COLOR_DEPTH 16
+
+/*=========================
+   STDLIB WRAPPER SETTINGS
+ *=========================*/
+
+#define LV_USE_STDLIB_MALLOC    LV_STDLIB_BUILTIN
+#define LV_USE_STDLIB_STRING    LV_STDLIB_BUILTIN
+#define LV_USE_STDLIB_SPRINTF   LV_STDLIB_BUILTIN
+
+#define LV_STDINT_INCLUDE       <stdint.h>
+#define LV_STDDEF_INCLUDE       <stddef.h>
+#define LV_STDBOOL_INCLUDE      <stdbool.h>
+#define LV_INTTYPES_INCLUDE     <inttypes.h>
+#define LV_LIMITS_INCLUDE       <limits.h>
+#define LV_STDARG_INCLUDE       <stdarg.h>
+
+#if LV_USE_STDLIB_MALLOC == LV_STDLIB_BUILTIN
+    /* Size of memory available for `lv_malloc()` in bytes (>= 2kB) */
+    #define LV_MEM_SIZE (128 * 1024U)          /* [bytes] */
+    #define LV_MEM_POOL_EXPAND_SIZE 0
+    #define LV_MEM_ADR 0     /* 0: unused */
+    #if LV_MEM_ADR == 0
+        #undef LV_MEM_POOL_INCLUDE
+        #undef LV_MEM_POOL_ALLOC
+    #endif
+#endif
+
+/*====================
+   HAL SETTINGS
+ *====================*/
+
+/* Default display refresh, input device read and animation step period */
+#define LV_DEF_REFR_PERIOD  20      /* [ms] */
+
+/* Default Dots Per Inch */
+#define LV_DPI_DEF 130              /* [px/inch] */
+
+/*=================
+ * OPERATING SYSTEM
+ *=================*/
+#define LV_USE_OS   LV_OS_NONE
+
+/*========================
+ * RENDERING CONFIGURATION
+ *========================*/
+
+#define LV_DRAW_BUF_STRIDE_ALIGN                1
+#define LV_DRAW_BUF_ALIGN                       4
+#define LV_DRAW_TRANSFORM_USE_MATRIX            0
+
+/* Layer buffer settings */
+#define LV_DRAW_LAYER_SIMPLE_BUF_SIZE    (24 * 1024)    /* [bytes] */
+#define LV_DRAW_LAYER_MAX_MEMORY 0  /* No limit by default [bytes] */
+
+/* Thread settings */
+#define LV_DRAW_THREAD_STACK_SIZE    (8 * 1024)         /* [bytes] */
+#define LV_DRAW_THREAD_PRIO LV_THREAD_PRIO_HIGH
+
+/* Software rendering */
+#define LV_USE_DRAW_SW 1
+#if LV_USE_DRAW_SW == 1
+    #define LV_DRAW_SW_SUPPORT_RGB565       1
+    #define LV_DRAW_SW_SUPPORT_RGB565_SWAPPED       1
+    #define LV_DRAW_SW_SUPPORT_RGB565A8     1
+    #define LV_DRAW_SW_SUPPORT_RGB888       1
+    #define LV_DRAW_SW_SUPPORT_XRGB8888     1
+    #define LV_DRAW_SW_SUPPORT_ARGB8888     1
+    #define LV_DRAW_SW_SUPPORT_ARGB8888_PREMULTIPLIED 1
+    #define LV_DRAW_SW_SUPPORT_L8           1
+    #define LV_DRAW_SW_SUPPORT_AL88         1
+    #define LV_DRAW_SW_SUPPORT_A8           1
+    #define LV_DRAW_SW_SUPPORT_I1           1
+
+    #define LV_DRAW_SW_I1_LUM_THRESHOLD 127
+    #define LV_DRAW_SW_DRAW_UNIT_CNT    1
+    #define LV_USE_DRAW_ARM2D_SYNC      0
+    #define LV_USE_NATIVE_HELIUM_ASM    0
+    #define LV_DRAW_SW_COMPLEX          1
+
+    #if LV_DRAW_SW_COMPLEX == 1
+        #define LV_DRAW_SW_SHADOW_CACHE_SIZE 0
+        #define LV_DRAW_SW_CIRCLE_CACHE_SIZE 4
+    #endif
+
+    #define LV_USE_DRAW_SW_ASM     LV_DRAW_SW_ASM_NONE
+    #define LV_USE_DRAW_SW_COMPLEX_GRADIENTS    0
+#endif
 
 /* Architecture specific settings */
 #define LV_ARCH_ESP32 1
 #define LV_USE_ARM_HELIUM 0
 #define LV_USE_ARM_NEON 0
 #define LV_USE_ARM_DSP 0
+#define LV_USE_DRAW_ARM2D_SYNC 0
+#define LV_USE_DRAW_ARM2D_ASYNC 0
+#define LV_USE_DRAW_ARM2D_DMA2D 0
+#define LV_USE_DRAW_ARM2D_GPU2D 0
+#define LV_USE_DRAW_ARM2D_GPU2D_ASYNC 0
+#define LV_USE_DRAW_ARM2D_GPU2D_DMA2D 0
+#define LV_USE_DRAW_ARM2D_GPU2D_DMA2D_ASYNC 0
 
-/* Color depth: 1 (1 byte per pixel), 8 (RGB332), 16 (RGB565), 32 (ARGB8888) */
-#define LV_COLOR_DEPTH 16
-#define LV_COLOR_16_SWAP 1
-#define LV_USE_PERF_MONITOR 1
+/* Enable LovyanGFX support */
+#define LV_USE_LOVYANGFX 1
 
+/*=======================
+ * FEATURE CONFIGURATION
+ *=======================*/
 
-/* Use more complex drawing routines to enable anti-aliasing, gradients, etc. */
-#define LV_DRAW_COMPLEX 1
-
-/* Default display refresh period. LVG will redraw changed areas with this period time */
-#define LV_DISP_DEF_REFR_PERIOD 30
-
-/* Input device read period in milliseconds */
-#define LV_INDEV_DEF_READ_PERIOD 30
-
-/* Enable anti-aliasing (lines, and radiuses will be smoothed) */
-#define LV_ANTIALIAS 1
-
-/* Enable the built-in memory manager */
-#define LV_MEM_CUSTOM 0
-#define LV_MEM_SIZE (128U * 1024U)    /* Size of the memory used by `lv_mem_alloc` */
-#define LV_MEM_ATTR                        /* Complier prefix for big arrays */
-#define LV_MEM_ADR_INV 0xFFFFFFFF          /* Invalid address */
-#define LV_MEM_AUTO_DEFRAG 1               /* Automatically defrag on free */
-
-/* Enable the built-in (v)snprintf functions */
-#define LV_SPRINTF_CUSTOM 0
-#define LV_SPRINTF_INCLUDE <stdio.h>
-#define lv_snprintf snprintf
-#define lv_vsnprintf vsnprintf
-
-/* Enable the built-in log module */
+/* Logging */
 #define LV_USE_LOG 1
-#define LV_LOG_LEVEL LV_LOG_LEVEL_INFO
-#define LV_LOG_PRINTF 1
+#if LV_USE_LOG
+    #define LV_LOG_LEVEL LV_LOG_LEVEL_INFO
+    #define LV_LOG_PRINTF 1
+    #define LV_LOG_USE_TIMESTAMP 1
+    #define LV_LOG_USE_FILE_LINE 1
+#endif
 
-/* Enable the built-in demo widgets */
-#define LV_USE_DEMO_WIDGETS 1
+/* Asserts */
+#define LV_USE_ASSERT_NULL          1
+#define LV_USE_ASSERT_MALLOC        1
+#define LV_USE_ASSERT_STYLE         0
+#define LV_USE_ASSERT_MEM_INTEGRITY 0
+#define LV_USE_ASSERT_OBJ           0
 
-/* Enable the built-in music demo */
+/* Debug */
+#define LV_USE_REFR_DEBUG 0
+#define LV_USE_LAYER_DEBUG 0
+#define LV_USE_PARALLEL_DRAW_DEBUG 0
 
-#define LV_USE_DEMO_BENCHMARK 1
+/* Font configuration */
+#define LV_FONT_MONTSERRAT_12 1
+#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_MONTSERRAT_16 1
+#define LV_FONT_MONTSERRAT_18 1
+#define LV_FONT_MONTSERRAT_20 1
+#define LV_FONT_MONTSERRAT_22 1
+#define LV_FONT_MONTSERRAT_24 1
+#define LV_FONT_MONTSERRAT_26 1
+#define LV_FONT_MONTSERRAT_28 1
+#define LV_FONT_MONTSERRAT_30 1
+#define LV_FONT_MONTSERRAT_32 1
+#define LV_FONT_MONTSERRAT_34 1
+#define LV_FONT_MONTSERRAT_36 1
+#define LV_FONT_MONTSERRAT_38 1
+#define LV_FONT_MONTSERRAT_40 1
+#define LV_FONT_MONTSERRAT_42 1
+#define LV_FONT_MONTSERRAT_44 1
+#define LV_FONT_MONTSERRAT_46 1
+#define LV_FONT_MONTSERRAT_48 1
 
-/* Enable the built-in widgets */
-#define LV_USE_WIDGETS 1
+#define LV_FONT_DEFAULT &lv_font_montserrat_14
+#define LV_FONT_FMT_TXT_LARGE 0
+#define LV_USE_FONT_COMPRESSED 0
+#define LV_USE_FONT_PLACEHOLDER 1
 
-/* Enable the built-in label widget */
-#define LV_USE_LABEL 1
+/* Text settings */
+#define LV_TXT_ENC LV_TXT_ENC_UTF8
+#define LV_TXT_BREAK_CHARS " ,.;:-_)]}"
+#define LV_TXT_LINE_BREAK_LONG_LEN 0
+#define LV_TXT_LINE_BREAK_LONG_PRE_MIN_LEN 3
+#define LV_TXT_LINE_BREAK_LONG_POST_MIN_LEN 3
+#define LV_USE_BIDI 0
+#define LV_USE_ARABIC_PERSIAN_CHARS 0
+#define LV_TXT_COLOR_CMD "#"
 
-/* Enable the built-in button widget */
-#define LV_USE_BTN 1
+/* Widgets */
+#define LV_WIDGETS_HAS_DEFAULT_VALUE  1
+#define LV_USE_ANIMIMG    1
+#define LV_USE_ARC        1
+#define LV_USE_BAR        1
+#define LV_USE_BUTTON     1
+#define LV_USE_BUTTONMATRIX  1
+#define LV_USE_CALENDAR   1
+#define LV_USE_CANVAS     1
+#define LV_USE_CHART      1
+#define LV_USE_CHECKBOX   1
+#define LV_USE_DROPDOWN   1
+#define LV_USE_IMAGE      1
+#define LV_USE_IMAGEBUTTON     1
+#define LV_USE_KEYBOARD   1
+#define LV_USE_LABEL      1
+#define LV_USE_LED        1
+#define LV_USE_LINE       1
+#define LV_USE_LIST       1
+#define LV_USE_MENU       1
+#define LV_USE_MSGBOX     1
+#define LV_USE_ROLLER     1
+#define LV_USE_SCALE      1
+#define LV_USE_SLIDER     1
+#define LV_USE_SPAN       1
+#define LV_USE_SPINBOX    1
+#define LV_USE_SPINNER    1
+#define LV_USE_SWITCH     1
+#define LV_USE_TABLE      1
+#define LV_USE_TABVIEW    1
+#define LV_USE_TEXTAREA   1
+#define LV_USE_TILEVIEW   1
+#define LV_USE_WIN        1
 
-/* Enable the built-in image widget */
-#define LV_USE_IMG 1
+/* Themes */
+#define LV_USE_THEME_DEFAULT 1
+#if LV_USE_THEME_DEFAULT
+    #define LV_THEME_DEFAULT_DARK 0
+    #define LV_THEME_DEFAULT_GROW 1
+    #define LV_THEME_DEFAULT_TRANSITION_TIME 80
+#endif
 
-/* Enable the built-in slider widget */
-#define LV_USE_SLIDER 1
+#define LV_USE_THEME_SIMPLE 1
+#define LV_USE_THEME_MONO 1
 
-/* Enable the built-in chart widget */
-#define LV_USE_CHART 1
+/* Layouts */
+#define LV_USE_FLEX 1
+#define LV_USE_GRID 1
 
-/* Enable the built-in table widget */
-#define LV_USE_TABLE 1
+/* File system */
+#define LV_USE_FS_FATFS 1
+#if LV_USE_FS_FATFS
+    #define LV_FS_FATFS_LETTER 'S'
+    #define LV_FS_FATFS_CACHE_SIZE 512
+#endif
 
-/* Enable the built-in textarea widget */
-#define LV_USE_TEXTAREA 1
+/*==================
+ * OTHERS
+ *==================*/
 
-/* Enable the built-in keyboard widget */
-#define LV_USE_KEYBOARD 1
+#define LV_USE_SNAPSHOT 0
+#define LV_USE_SYSMON   0
+#define LV_USE_PROFILER 0
+#define LV_USE_MONKEY 0
+#define LV_USE_GRIDNAV 0
+#define LV_USE_FRAGMENT 0
+#define LV_USE_IMGFONT 0
+#define LV_USE_OBSERVER 1
+#define LV_USE_IME_PINYIN 0
+#define LV_USE_FILE_EXPLORER 0
+#define LV_USE_FONT_MANAGER 0
+#define LV_USE_TEST 0
+#define LV_USE_XML 0
+#define LV_USE_COLOR_FILTER 0
 
-/* Enable the built-in message box widget */
-#define LV_USE_MSGBOX 1
+/*==================
+ * DEVICES
+ *==================*/
 
-/* Enable the built-in spinbox widget */
-#define LV_USE_SPINBOX 1
+#define LV_USE_TFT_ESPI 0
+#define LV_USE_EVDEV 0
+#define LV_USE_LIBINPUT 0
+#define LV_USE_ST7735 0
+#define LV_USE_ST7789 0
+#define LV_USE_ST7796 0
+#define LV_USE_ILI9341 0
+#define LV_USE_FT81X 0
 
-/* Enable the built-in spinner widget */
-#define LV_USE_SPINNER 1
+/*=====================
+* BUILD OPTIONS
+*======================*/
 
-/* Enable the built-in calendar widget */
-#define LV_USE_CALENDAR 1
+#define LV_BUILD_EXAMPLES 1
+#define LV_BUILD_DEMOS 1
 
-/* Enable the built-in canvas widget */
-#define LV_USE_CANVAS 1
+/*===================
+ * DEMO USAGE
+ ====================*/
 
-/* Enable the built-in list widget */
-#define LV_USE_LIST 1
-
-/* Enable the built-in menu widget */
-#define LV_USE_MENU 1
-
-/* Enable the built-in meter widget */
-#define LV_USE_METER 1
-
-/* Enable the built-in msgbox widget */
-#define LV_USE_MSGBOX 1
-
-/* Enable the built-in tabview widget */
-#define LV_USE_TABVIEW 1
-
-/* Enable the built-in tileview widget */
-#define LV_USE_TILEVIEW 1
-
-/* Enable the built-in win widget */
-#define LV_USE_WIN 1
-
-/* Enable the built-in color picker widget */
-#define LV_USE_COLORWHEEL 1
-
-/* Enable the built-in led widget */
-#define LV_USE_LED 1
+#if LV_BUILD_DEMOS
+    #define LV_USE_DEMO_WIDGETS 1
+    #define LV_USE_DEMO_KEYPAD_AND_ENCODER 0
+    #define LV_USE_DEMO_BENCHMARK 1
+    #define LV_USE_DEMO_RENDER 0
+    #define LV_USE_DEMO_STRESS 0
+    #define LV_USE_DEMO_MUSIC 0
+    #define LV_USE_DEMO_VECTOR_GRAPHIC 0
+    #define LV_USE_DEMO_FLEX_LAYOUT 0
+    #define LV_USE_DEMO_MULTILANG 0
+    #define LV_USE_DEMO_TRANSFORM 0
+    #define LV_USE_DEMO_SCROLL 0
+    #define LV_USE_DEMO_EBIKE 0
+    #define LV_USE_DEMO_HIGH_RES 0
+    #define LV_USE_DEMO_SMARTWATCH 0
+#endif
 
 #endif /*LV_CONF_H*/ 
--- src/main.cpp
+++ src/main.cpp
@@ -1,248 +1,248 @@
 #include <Arduino.h>
 #include <LovyanGFX.h>
-#include "LGFX_ESP32S3_RGB_TFT_SPI_ST7701_GT911.h"
-#include "Pet.hpp"
+#include "LGFX_ESP32S3_RGB_TFT_SPI_ST7701_GT911.h" // Your specific display configuration
+#include "Pet.hpp" // Your Pet class definition
 
+// LVGL includes
+#include <lvgl.h>
+
+// --- LVGL Configuration ---
+#define LV_HOR_RES_MAX 480
+#define LV_VER_RES_MAX 480
+#define LVGL_BUFFER_SIZE (LV_HOR_RES_MAX * LV_VER_RES_MAX / 4)
+
+// LVGL v8.x uses lv_disp_draw_buf_t and lv_color_t
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[LVGL_BUFFER_SIZE];
+
+// --- Global Objects ---
 LGFX gfx;
 Pet myPet("Buddy");
 
-// Store previous values to detect changes
-struct PetStatus {
-    PetState state;
-    uint8_t health;
-    uint8_t hunger;
-    uint8_t happiness;
-    uint8_t energy;
-    uint8_t cleanliness;
-} previousStatus;
+// --- LVGL UI Objects ---
+lv_obj_t* screen;
+lv_obj_t* nameLabel;
+lv_obj_t* stateLabel;
+lv_obj_t* healthBar;
+lv_obj_t* hungerBar;
+lv_obj_t* happinessBar;
+lv_obj_t* energyBar;
+lv_obj_t* cleanlinessBar;
 
-// Button definitions
-struct Button {
-    int16_t x;
-    int16_t y;
-    int16_t width;
-    int16_t height;
-    const char* label;
-    uint16_t color;
-    uint16_t textColor;
-};
-
-// Define buttons
-const Button buttons[] = {
-    {10, 300, 100, 40, "Feed", TFT_RED, TFT_WHITE},
-    {120, 300, 100, 40, "Play", TFT_GREEN, TFT_WHITE},
-    {230, 300, 100, 40, "Sleep", TFT_BLUE, TFT_WHITE},
-    {340, 300, 100, 40, "Clean", TFT_YELLOW, TFT_BLACK}
-};
-
-// Timing variables
-unsigned long lastUpdateTime = 0;
-unsigned long lastTouchTime = 0;
-const unsigned long UPDATE_INTERVAL = 1000;  // 1 second
-const unsigned long TOUCH_DEBOUNCE = 200;    // 200ms
-
-// Function to draw a button
-void drawButton(const Button& btn, bool pressed = false) {
-    // Draw button background
-    gfx.fillRoundRect(btn.x, btn.y, btn.width, btn.height, 8, pressed ? gfx.color565(100, 100, 100) : btn.color);
-    
-    // Draw button border
-    gfx.drawRoundRect(btn.x, btn.y, btn.width, btn.height, 8, TFT_WHITE);
-    
-    // Draw button label
-    gfx.setTextColor(btn.textColor);
-    gfx.setTextSize(2);
-    
-    // Center the text
-    int16_t textWidth = gfx.textWidth(btn.label);
-    int16_t textX = btn.x + (btn.width - textWidth) / 2;
-    int16_t textY = btn.y + (btn.height - 16) / 2;  // 16 is approximate text height
-    
-    gfx.setCursor(textX, textY);
-    gfx.print(btn.label);
+// --- LVGL Callbacks (Updated for LVGL v8.x API) ---
+void lv_disp_flush_cb(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p) {
+    gfx.pushImage(area->x1, area->y1, area->x2 - area->x1 + 1, area->y2 - area->y1 + 1, (lgfx::rgb565_t*)color_p);
+    lv_disp_flush_ready(disp_drv);
 }
 
-// Function to draw static UI elements
-void drawStaticElements() {
-    // Draw labels
-    gfx.setCursor(10, 10);
-    gfx.setTextColor(TFT_WHITE);
-    gfx.setTextSize(2);
-    gfx.print("Name: ");
-    gfx.print(myPet.getName());
-    
-    gfx.setCursor(10, 40);
-    gfx.print("State: ");
-    
-    // Draw static parts of status bars with initial values
-    int y = 100;
-    int barWidth = 200;
-    
-    const char* labels[] = {"Health:", "Hunger:", "Happy:", "Energy:", "Clean:"};
-    uint16_t colors[] = {TFT_RED, TFT_ORANGE, TFT_YELLOW, TFT_GREEN, TFT_BLUE};
-    uint8_t values[] = {
-        myPet.getHealth(),
-        myPet.getHunger(),
-        myPet.getHappiness(),
-        myPet.getEnergy(),
-        myPet.getCleanliness()
+void lv_touchpad_read_cb(lv_indev_drv_t *indev_drv, lv_indev_data_t *data) {
+    uint16_t touchX, touchY;
+    if (gfx.getTouch(&touchX, &touchY)) {
+        data->state = LV_INDEV_STATE_PR;
+        data->point.x = touchX;
+        data->point.y = touchY;
+    } else {
+        data->state = LV_INDEV_STATE_REL;
+    }
+}
+
+// --- LVGL Event Handlers for Buttons ---
+void feed_btn_event_cb(lv_event_t *e) {
+    lv_event_code_t code = lv_event_get_code(e);
+    if (code == LV_EVENT_CLICKED) {
+        myPet.feed();
+        Serial.println("Feed button clicked!");
+    }
+}
+
+void play_btn_event_cb(lv_event_t *e) {
+    lv_event_code_t code = lv_event_get_code(e);
+    if (code == LV_EVENT_CLICKED) {
+        myPet.play();
+        Serial.println("Play button clicked!");
+    }
+}
+
+void sleep_btn_event_cb(lv_event_t *e) {
+    lv_event_code_t code = lv_event_get_code(e);
+    if (code == LV_EVENT_CLICKED) {
+        myPet.sleep();
+        Serial.println("Sleep button clicked!");
+    }
+}
+
+void clean_btn_event_cb(lv_event_t *e) {
+    lv_event_code_t code = lv_event_get_code(e);
+    if (code == LV_EVENT_CLICKED) {
+        myPet.clean();
+        Serial.println("Clean button clicked!");
+    }
+}
+
+// --- UI Creation Function ---
+void create_ui() {
+    screen = lv_obj_create(NULL);
+    lv_scr_load(screen);
+
+    // Set background color
+    lv_obj_set_style_bg_color(screen, lv_color_hex(0x000000), LV_PART_MAIN);
+
+    // Pet Name Label
+    nameLabel = lv_label_create(screen);
+    lv_label_set_text_fmt(nameLabel, "Name: %s", myPet.getName());
+    lv_obj_set_style_text_color(nameLabel, lv_color_hex(0xFFFFFF), LV_PART_MAIN);
+    lv_obj_set_style_text_font(nameLabel, &lv_font_montserrat_14, LV_PART_MAIN);
+    lv_obj_set_pos(nameLabel, 10, 10);
+
+    // Pet State Label
+    stateLabel = lv_label_create(screen);
+    lv_label_set_text_fmt(stateLabel, "State: %d", (int)myPet.getState());
+    lv_obj_set_style_text_color(stateLabel, lv_color_hex(0xFFFFFF), LV_PART_MAIN);
+    lv_obj_set_style_text_font(stateLabel, &lv_font_montserrat_14, LV_PART_MAIN);
+    lv_obj_set_pos(stateLabel, 10, 50);
+
+    // Status Bars
+    lv_obj_t* labels[] = {
+        lv_label_create(screen), lv_label_create(screen), lv_label_create(screen),
+        lv_label_create(screen), lv_label_create(screen)
     };
     
-    for(int i = 0; i < 5; i++) {
-        gfx.setCursor(10, y + (i * 30));
-        gfx.print(labels[i]);
-        
-        // Draw background
-        gfx.fillRect(100, y + (i * 30), barWidth, 20, TFT_DARKGREY);
-        
-        // Draw initial value
-        int width = (barWidth * values[i]) / 100;
-        if (width > 0) {
-            gfx.fillRect(100, y + (i * 30), width, 20, colors[i]);
-        }
-    }
-    
-    // Draw all buttons
-    for(const auto& btn : buttons) {
-        drawButton(btn);
-    }
-}
-
-// Function to update dynamic UI elements
-void drawPetStatus() {
-    // Only update state text if it changed
-    if (previousStatus.state != myPet.getState()) {
-        gfx.fillRect(100, 40, 200, 20, TFT_BLACK);  // Clear previous state
-        gfx.setCursor(100, 40);
-        gfx.setTextColor(TFT_WHITE);
-        gfx.print((int)myPet.getState());
-        previousStatus.state = myPet.getState();
-    }
-    
-    // Update status bars only if values changed
-    int y = 100;
-    int barWidth = 200;
-    
-    // Helper function to update a single bar
-    auto updateBar = [&](uint8_t current, uint8_t& previous, int yPos, uint16_t color) {
-        if (current != previous) {
-            // Calculate new width
-            int newWidth = (barWidth * current) / 100;
-            int oldWidth = (barWidth * previous) / 100;
-            
-            // If new value is smaller, clear the excess
-            if (newWidth < oldWidth) {
-                gfx.fillRect(100 + newWidth, yPos, oldWidth - newWidth, 20, TFT_DARKGREY);
-            }
-            // If new value is larger, fill the new area
-            if (newWidth > oldWidth) {
-                gfx.fillRect(100 + oldWidth, yPos, newWidth - oldWidth, 20, color);
-            }
-            previous = current;
-        }
+    const char* bar_labels[] = {"Health:", "Hunger:", "Happy:", "Energy:", "Clean:"};
+    lv_color_t bar_colors[] = {
+        lv_color_hex(0xFF0000),
+        lv_color_hex(0xFFA500),
+        lv_color_hex(0xFFFF00),
+        lv_color_hex(0x00FF00),
+        lv_color_hex(0x0000FF)
     };
-    
-    // Update each bar
-    updateBar(myPet.getHealth(), previousStatus.health, y, TFT_RED);
-    updateBar(myPet.getHunger(), previousStatus.hunger, y + 30, TFT_ORANGE);
-    updateBar(myPet.getHappiness(), previousStatus.happiness, y + 60, TFT_YELLOW);
-    updateBar(myPet.getEnergy(), previousStatus.energy, y + 90, TFT_GREEN);
-    updateBar(myPet.getCleanliness(), previousStatus.cleanliness, y + 120, TFT_BLUE);
+
+    int y_start_bars = 100;
+    int bar_height = 20;
+    int bar_width = 250;
+    int spacing = 30;
+
+    for (int i = 0; i < 5; ++i) {
+        lv_label_set_text(labels[i], bar_labels[i]);
+        lv_obj_set_style_text_color(labels[i], lv_color_hex(0xFFFFFF), LV_PART_MAIN);
+        lv_obj_set_style_text_font(labels[i], &lv_font_montserrat_14, LV_PART_MAIN);
+        lv_obj_set_pos(labels[i], 10, y_start_bars + i * spacing);
+
+        lv_obj_t* current_bar = lv_bar_create(screen);
+        lv_obj_set_size(current_bar, bar_width, bar_height);
+        lv_obj_set_pos(current_bar, 100, y_start_bars + i * spacing);
+        lv_bar_set_range(current_bar, 0, 100);
+
+        lv_obj_set_style_bg_color(current_bar, bar_colors[i], LV_PART_INDICATOR);
+        lv_obj_set_style_bg_color(current_bar, lv_color_hex(0x404040), LV_PART_MAIN);
+        lv_obj_set_style_radius(current_bar, LV_RADIUS_CIRCLE, LV_PART_MAIN | LV_PART_INDICATOR);
+
+        if (i == 0) healthBar = current_bar;
+        else if (i == 1) hungerBar = current_bar;
+        else if (i == 2) happinessBar = current_bar;
+        else if (i == 3) energyBar = current_bar;
+        else if (i == 4) cleanlinessBar = current_bar;
+    }
+
+    // Buttons
+    lv_obj_t* btns[] = {
+        lv_btn_create(screen), lv_btn_create(screen), lv_btn_create(screen), lv_btn_create(screen)
+    };
+    lv_obj_t* btn_labels[] = {
+        lv_label_create(btns[0]), lv_label_create(btns[1]), lv_label_create(btns[2]), lv_label_create(btns[3])
+    };
+    const char* btn_texts[] = {"Feed", "Play", "Sleep", "Clean"};
+    lv_event_cb_t btn_cbs[] = {feed_btn_event_cb, play_btn_event_cb, sleep_btn_event_cb, clean_btn_event_cb};
+    lv_color_t btn_colors[] = {
+        lv_color_hex(0xFF0000),
+        lv_color_hex(0x00FF00),
+        lv_color_hex(0x0000FF),
+        lv_color_hex(0xFFFF00)
+    };
+
+    int btn_width = 100;
+    int btn_height = 40;
+    int btn_start_x = 10;
+    int btn_y = 300;
+    int btn_spacing = 110;
+
+    for (int i = 0; i < 4; ++i) {
+        lv_obj_set_size(btns[i], btn_width, btn_height);
+        lv_obj_set_pos(btns[i], btn_start_x + i * btn_spacing, btn_y);
+        
+        lv_obj_set_style_bg_color(btns[i], btn_colors[i], LV_PART_MAIN);
+        lv_obj_set_style_radius(btns[i], 8, LV_PART_MAIN);
+
+        lv_label_set_text(btn_labels[i], btn_texts[i]);
+        lv_obj_set_style_text_color(btn_labels[i], lv_color_hex(0xFFFFFF), LV_PART_MAIN);
+        lv_obj_set_style_text_font(btn_labels[i], &lv_font_montserrat_14, LV_PART_MAIN);
+        lv_obj_center(btn_labels[i]);
+
+        lv_obj_add_event_cb(btns[i], btn_cbs[i], LV_EVENT_CLICKED, NULL);
+    }
 }
 
-// Function to check if a point is inside a button
-bool isPointInButton(int16_t x, int16_t y, const Button& btn) {
-    return x >= btn.x && x < btn.x + btn.width &&
-           y >= btn.y && y < btn.y + btn.height;
+void update_ui_pet_status() {
+    lv_label_set_text_fmt(stateLabel, "State: %d", (int)myPet.getState());
+
+    lv_bar_set_value(healthBar, myPet.getHealth(), LV_ANIM_OFF);
+    lv_bar_set_value(hungerBar, myPet.getHunger(), LV_ANIM_OFF);
+    lv_bar_set_value(happinessBar, myPet.getHappiness(), LV_ANIM_OFF);
+    lv_bar_set_value(energyBar, myPet.getEnergy(), LV_ANIM_OFF);
+    lv_bar_set_value(cleanlinessBar, myPet.getCleanliness(), LV_ANIM_OFF);
 }
 
 void setup() {
     Serial.begin(115200);
-    Serial.println("Starting...");
-    
+    Serial.println("Starting ESPets with LVGL...");
+
     gfx.begin();
     gfx.setRotation(0);
     gfx.setBrightness(128);
-    
-    // Initial display setup
-    gfx.setTextSize(2);
-    gfx.setTextColor(TFT_WHITE);
-    
-    // Clear screen
     gfx.fillScreen(TFT_BLACK);
-    
-    // Initialize previous status
-    previousStatus.state = myPet.getState();
-    previousStatus.health = myPet.getHealth();
-    previousStatus.hunger = myPet.getHunger();
-    previousStatus.happiness = myPet.getHappiness();
-    previousStatus.energy = myPet.getEnergy();
-    previousStatus.cleanliness = myPet.getCleanliness();
-    
-    // Draw initial static elements
-    drawStaticElements();
-    
-    // Force initial display of all values
-    drawPetStatus();
-    
-    Serial.println("Setup complete");
+
+    lv_init();
+
+    // Initialize LVGL drawing buffers (v8.x style)
+    lv_disp_draw_buf_init(&draw_buf, buf, NULL, LVGL_BUFFER_SIZE);
+
+    // Register display driver (v8.x style)
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.hor_res = LV_HOR_RES_MAX;
+    disp_drv.ver_res = LV_VER_RES_MAX;
+    disp_drv.flush_cb = lv_disp_flush_cb;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    // Register input device driver (v8.x style)
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = lv_touchpad_read_cb;
+    lv_indev_drv_register(&indev_drv);
+
+    create_ui();
+    update_ui_pet_status();
+
+    Serial.println("Setup complete. UI created.");
 }
+
+unsigned long lastPetUpdateTime = 0;
+const unsigned long PET_UPDATE_INTERVAL = 1000;
 
 void loop() {
     unsigned long currentTime = millis();
-    
-    // Update pet state and display at regular intervals
-    if (currentTime - lastUpdateTime >= UPDATE_INTERVAL) {
+
+    if (currentTime - lastPetUpdateTime >= PET_UPDATE_INTERVAL) {
         myPet.update();
-        drawPetStatus();
-        lastUpdateTime = currentTime;
-        
-        // Print status to Serial for debugging
+        update_ui_pet_status();
+        lastPetUpdateTime = currentTime;
+
         Serial.printf("State: %d, Health: %d, Hunger: %d, Happy: %d, Energy: %d, Clean: %d\n",
                      (int)myPet.getState(), myPet.getHealth(), myPet.getHunger(),
                      myPet.getHappiness(), myPet.getEnergy(), myPet.getCleanliness());
     }
-    
-    // Check for touch input with debounce
-    if (currentTime - lastTouchTime >= TOUCH_DEBOUNCE) {
-        uint16_t x, y;
-        if (gfx.getTouch(&x, &y)) {
-            Serial.printf("Touch at: %d, %d\n", x, y);
-            lastTouchTime = currentTime;
-            
-            // Check which button was pressed
-            for(int i = 0; i < 4; i++) {
-                if (isPointInButton(x, y, buttons[i])) {
-                    Serial.printf("Button %d pressed\n", i);
-                    
-                    // Draw pressed button
-                    drawButton(buttons[i], true);
-                    
-                    // Perform action based on button index
-                    switch(i) {
-                        case 0: 
-                            myPet.feed(); 
-                            Serial.println("Feed action");
-                            break;
-                        case 1: 
-                            myPet.play(); 
-                            Serial.println("Play action");
-                            break;
-                        case 2: 
-                            myPet.sleep(); 
-                            Serial.println("Sleep action");
-                            break;
-                        case 3: 
-                            myPet.clean(); 
-                            Serial.println("Clean action");
-                            break;
-                    }
-                    
-                    // Redraw button in normal state
-                    drawButton(buttons[i], false);
-                    break;
-                }
-            }
-        }
-    }
+
+    lv_timer_handler();
+    delay(5);
 }
